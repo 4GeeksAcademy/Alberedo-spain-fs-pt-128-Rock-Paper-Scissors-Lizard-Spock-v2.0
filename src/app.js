0da39d951b5c6d44db1cb9e7e6f3bcc9599ede4a
Alberto Madrid <albertomadrid93@gmail.com>
2025-11-21 10:17:24 +0000

    Ejercicio realizado en JS

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-import "bootstrap";
-import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const choises = ["rock", "paper", "scissors", "lizard", "spock"]
+const rules = {
+  // Piedra gana a tijeras y a lagartija
+  rock: ['scissors', "lizard"],
+  // Papel gana a piedra y a Spock
+  paper: ["rock", "spock"],
+  // Tijeras gana a papel y a lagartija
+  scissors: ["paper", "lizard"],
+  // Lagartija gana a Spock y a papel
+  lizard: ["spock", "paper"],
+  // Spock gana a tijeras y a pieda
+  spock: ["scissors", "rock"]
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const computerChoise = choises[Math.floor(Math.random()*choises.length)]
+console.log(`El ordenador ha elegido: ${computerChoise}`);
+
+const play=(userChoise)=>{
+    if(userChoise === computerChoise){
+      console.log("Habeis tenido un empate! Vuelve a intentarlo");
+    }
+    else if(rules[userChoise].includes(computerChoise)){
+      console.log("Enhorabuena! Ganastes!")
+    }
+    else{
+      console.log("Lo siento, has perdido...");
+      
+    }
+}
+
+play("lizard")
