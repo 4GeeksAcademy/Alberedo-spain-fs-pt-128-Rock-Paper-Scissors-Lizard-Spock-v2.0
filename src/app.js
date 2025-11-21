29056b27a09aeee30d012131d16f01c4e98a4ce9
Alberto Madrid <albertomadrid93@gmail.com>
2025-11-21 11:43:44 +0000

    Proyecto terminado

--- src/app.js
+++ src/app.js
@@ -1,3 +1,10 @@
+import "bootstrap";
+import "./style.css";
+
+
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+
 
 
 const choises = ["rock", "paper", "scissors", "lizard", "spock"]
@@ -13,21 +20,62 @@ const rules = {
   // Spock gana a tijeras y a pieda
   spock: ["scissors", "rock"]
 }
+let computerChoise
 
-const computerChoise = choises[Math.floor(Math.random()*choises.length)]
-console.log(`El ordenador ha elegido: ${computerChoise}`);
-
-const play=(userChoise)=>{
-    if(userChoise === computerChoise){
-      console.log("Habeis tenido un empate! Vuelve a intentarlo");
-    }
-    else if(rules[userChoise].includes(computerChoise)){
-      console.log("Enhorabuena! Ganastes!")
-    }
-    else{
-      console.log("Lo siento, has perdido...");
-      
-    }
+function randomChoise() {
+  computerChoise = choises[Math.floor(Math.random() * choises.length)]
+  console.log(`El ordenador ha elegido: ${computerChoise}`);
 }
 
-play("lizard")
+const play = (userChoise) => {
+  if (userChoise === computerChoise) {
+    console.log("Habeis tenido un empate! Vuelve a intentarlo");
+  }
+  else if (rules[userChoise].includes(computerChoise)) {
+    console.log("Enhorabuena! Ganastes!")
+    document.getElementById("result").innerHTML = "🎉¡¡Ganastes!! 🎉";
+  }
+  else {
+    console.log("Lo siento, has perdido...");
+    document.getElementById("result").innerHTML = "💀¡¡Perdistes!!💀";
+
+  }
+}
+
+document.getElementById("rock").addEventListener("click", function () {
+  document.getElementById("userChoise").innerHTML = "rock";
+  randomChoise()
+  document.getElementById("computerChoise").innerHTML = computerChoise;
+  play("rock")
+  document.getElementById("resultDiv").classList.remove("d-none")
+});
+
+document.getElementById("paper").addEventListener("click", function () {
+  document.getElementById("userChoise").innerHTML = "paper";
+  randomChoise()
+  document.getElementById("computerChoise").innerHTML = computerChoise;
+  play("paper")
+});
+
+document.getElementById("scissors").addEventListener("click", function () {
+  document.getElementById("userChoise").innerHTML = "scissors";
+  play("scissors")
+  randomChoise()
+  document.getElementById("computerChoise").innerHTML = computerChoise;
+});
+
+document.getElementById("lizard").addEventListener("click", function () {
+  document.getElementById("userChoise").innerHTML = "lizard";
+  play("lizard")
+  randomChoise()
+  document.getElementById("computerChoise").innerHTML = computerChoise;
+});
+
+document.getElementById("spock").addEventListener("click", function () {
+  document.getElementById("userChoise").innerHTML = "spock";
+  play("spock")
+  randomChoise()
+  document.getElementById("computerChoise").innerHTML = computerChoise;
+});
+
+
